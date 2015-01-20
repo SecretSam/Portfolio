93247afcbc4e880fd57afcac086b40c7cc8ff744
SecretSam <samantha.scharrer@gmail.com>
2015-01-20 01:29:31 -0700

    navbar brand link

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
 	$("#home").on("click", function () {
 		$( "#MainContent" ).load( "home.html" );
 	});
+	$(".navbar-brand").on("click", function () {
+		$( "#MainContent" ).load( "home.html" );
+	});
 	$("#about").on("click", function () {
 		$( "#MainContent" ).load( "about.html" );
 	});
