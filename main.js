de3642b66533d27c621d8106d0c571226b421b10
SecretSam <samantha.scharrer@gmail.com>
2015-01-19 23:06:18 -0700

    Navbar links in main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+$("#home").on("click", function () {
+	$( "#MainContent" ).load( "home.html" );
+});
 $("#about").on("click", function () {
 	$( "#MainContent" ).load( "about.html" );
+});
+$("#skills").on("click", function () {
+	$( "#MainContent" ).load( "skills.html" );
+});
+$("#articles").on("click", function () {
+	$( "#MainContent" ).load( "articles.html" );
 });
