fd9aae050f15899fbf30edf22021fc4db57aa7ee
SecretSam <samantha.scharrer@gmail.com>
2015-01-19 23:50:06 -0700

    Added alert

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 		$( "#MainContent" ).load( "home.html" );
 	});
 	$("#about").on("click", function () {
+		alert( "Aiko" );
 		$( "#MainContent" ).load( "about.html" );
 	});
 	$("#skills").on("click", function () {
